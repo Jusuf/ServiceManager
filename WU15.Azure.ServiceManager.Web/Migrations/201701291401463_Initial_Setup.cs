df907591550fcddfbed97e009dc8038bbcddcfa1
jusuf_b <jusuf_b@hotmail.com>
2017-01-29 15:35:07 +0100

    Migrations and connection string

---
+++ WU15.Azure.ServiceManager.Web/Migrations/201701291401463_Initial_Setup.cs
@@ -0,0 +1,16 @@
+namespace WU15.Azure.ServiceManager.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial_Setup : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
