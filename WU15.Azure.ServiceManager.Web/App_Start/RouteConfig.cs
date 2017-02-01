7bb73acb56bbf3530b96b3c8bf519c94e2f6f736
jusuf_b <jusuf_b@hotmail.com>
2017-02-01 16:52:10 +0100

    Route namespaces

--- WU15.Azure.ServiceManager.Web/App_Start/RouteConfig.cs
+++ WU15.Azure.ServiceManager.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace WU15.Azure.ServiceManager.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WU15.Azure.ServiceManager.Web.Controlers" }
             );
         }
     }
